71440e36fd508aeb41b353be9974f884e31b945a
Brian Cheung <bdcheung@gmail.com>
2013-05-21 21:40:51 -0400

    Finalized all required functionality.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,9 @@ class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
   belongs_to :merchant
-  attr_accessible :quantity
+  attr_accessible :quantity, :product, :merchant, :user
 
-  def total_cost
+  def total_revenue
   	product.price * quantity
   end
 
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,16 +1,23 @@
 class Upload
+	@@gross_revenue = 0
 	def self.import(file)
 		CSV.foreach(file.path, headers: true, col_sep:"\t") do |row|
-			u = User.new
-				u.name = row[0]
+			u = User.where(name: row[0]).first_or_initialize
 			u.save
 
-			p = Product.new
-				p.description = row[1]
-				p.price = row[2]
+			p = Product.where(description: row[1], price: row[2]).first_or_initialize
 			p.save
 
-			
+			m = Merchant.where(name: row[5], address: row[4]).first_or_initialize
+			m.save
+
+			o = Order.create(user: u, product: p, merchant: m, quantity: row[3])
+
+			@@gross_revenue += o.total_revenue
 		end
+	end
+
+	def self.gross_revenue
+		@@gross_revenue
 	end
 end
